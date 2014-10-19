6ac8ae31efe84d6e5799d81b9229a26862e8a0e4
Chris Hampu <chrishampu@outlook.com>
2014-10-19 13:17:21 -0700

    Footer now opens more quickly.

--- Common/js/Footer.js
+++ Common/js/Footer.js
@@ -2,14 +2,14 @@ $('.log-footer').click(function() {
 	if ($(this).height() > 40) {
 		$(this).animate({ 
 		  height: "40px"
-		}, 600, 'swing', function() {
+		}, 400, 'swing', function() {
 			$(this).addClass('footer-closed');
 		});
 
 	} else {
 		$(this).animate({ 
 		  height: "450px" 
-		}, 600, 'swing', function() {
+		}, 400, 'swing', function() {
 			   $(this).removeClass('footer-closed');
 		});
 	}
