990ac38b0c93d246a3f5f837b89da6a2ca088f51
JakeCooper <Jake@JakeCooper.me>
2014-10-20 23:28:16 -0700

    Committed code for non-scrolling filter buttons.

--- Common/js/Footer.js
+++ Common/js/Footer.js
@@ -7,11 +7,11 @@ $('#log-footer').click(function(event) {
 	$(this).toggleClass('footer-closed');
 
 	if ($(this).height() > 400) {
-		$(this).css("overflow-y","scroll");
+		$('#logger-view').css("overflow-y","scroll");
 
 	}
 	else {
-		$(this).css("overflow-y","hidden");
+		$('#logger-view').css("overflow-y","hidden");
 	}
 });
 
