4f9f7e96e7d511728f4efeeb5461168e70a25d66
Charlie Friend <charles.d.friend@gmail.com>
2015-12-01 19:10:09 -0800

    Added a model for messages sent over the network.

---
+++ SystemAnalyst/js/models/NetMessageModel.js
@@ -0,0 +1,32 @@
+define([
+	'jquery',
+	'underscore',
+	'backbone', 
+	'Network'
+], function($, _, Backbone, Network){
+
+	/**
+	  * This model will hold a row of log message text sent over our network.
+	  * @extends BackBone.Model
+	  */
+	var NetMessageModel = Backbone.Model.extend({
+
+		initialize: function() {
+
+		},
+
+		// Default values if they aren't provided during initialization of the object
+		defaults: {
+			source: Network.ClientTypeEnum.Unknown,
+			severity: 'debug',
+			timestamp: _.now(),
+			display: true
+		}
+	});
+
+	/**
+	  * Returns the object containing our extended Model
+	  * @return
+	  */
+	return NetMessageModel;
+});
