ee342307d8628c7bc064c5caef9fba4608332e59
Charlie Friend <charles.d.friend@gmail.com>
2016-01-03 14:58:13 -0800

    Add render function to SAMonitorView Minor syntax fixes

--- SystemAnalyst/js/SAMonitorView.js
+++ SystemAnalyst/js/SAMonitorView.js
@@ -23,6 +23,11 @@ define([
 		"ip": true
 	};
 
+	// function to determine whether or not message is displayed
+	var toBeDisplayed = function(msg) {
+		return !(msg.get("message") == undefined) && msg.get("display") == true;
+	}
+
 	/**
 	  * System Analyst Monitor view - gets messages sent over our
 	  * network and displays them in the SA client.
@@ -39,11 +44,17 @@ define([
 			//listen for toggle button events
 			$('.sa-sev-toggle').on('click', $.proxy(this.toggleSev, this));
 			$('.sa-sys-toggle').on('click', $.proxy(this.toggleSys, this));
+
+			//render if there are any pre-existing messages in collection
+			if (this.collection.length == 0) {
+				this.render();
+			}
+			
 		},
 		
 		add: function(newMessage) {
 			//check that the message is displayed and exists
-			if (newMessage.get("display") === true && newMessage.get("message") !== undefined) {
+			if (toBeDisplayed(newMessage)) {
 
 				var view = new SAMessageView({model: newMessage});
 				this.$el.append(view.render().el); //append message to div
@@ -51,15 +62,28 @@ define([
 			}
 		},
 
+		render: function() {
+
+			var mhtml = this.collection.filter(toBeDisplayed) //filter out hidden or invalid messages
+			.map( function(msg) {return (new SAMessageView({model : msg})).render().el;} ) //get message view objects
+			.reduce( function(html, view) { return html + view.outerHTML; }, "" ); //concatenate all SA message HTML
+
+			//clear and add HTML chunk to view
+			this.$el.html("").append(mhtml);
+			console.log(this.$el.html);
+
+			return this; //return view object
+		},
+
 		toggleSev: function(ev) {
 
 			//name of severity to be toggled
-			target = ev.target.id.split("-")[1];
+			var target = ev.target.id.split("-")[1];
 			console.log(target);
 
 			//toggle display variable for target
 			MsgVis[target] = !MsgVis[target];
-			val = MsgVis[target] //current display value
+			var val = MsgVis[target] //current display value
 			console.log(MsgVis[target]);
 
 			this.collection.each(function(msg) {
@@ -67,6 +91,8 @@ define([
 					msg.set({"display" : val});
 				}
 			});
+
+			this.render();
 		},
 
 		toggleSys: function(ev) {
